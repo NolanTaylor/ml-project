d5575c4f0cea3a56ac20eb864b303dfd3a75351a
Nolan Taylor <itsapotato2@gmail.com>
2019-09-06 13:28:47 -0700

    contours drawn and saved (kinda)

--- those_canny_line_things.py
+++ those_canny_line_things.py
@@ -1,11 +1,38 @@
 import cv2
 import pandas
+import numpy as np
 
 cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("test")
 
 img_counter = 0
+
+def drawLines(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    kernel_size = 5
+    blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+
+    low_threshold = 50
+    high_threshold = 150
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+    rho = 1  # distance resolution in pixels of the Hough grid
+    theta = np.pi / 180  # angular resolution in radians of the Hough grid
+    threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 50  # minimum number of pixels making up a line
+    max_line_gap = 20  # maximum gap in pixels between connectable line segments
+    line_image = np.copy(img) * 0  # creating a blank to draw lines on
+
+    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                min_line_length, max_line_gap)
+
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+
+    lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
 
 while True:
     ret, frame = cam.read()
@@ -26,16 +53,19 @@ while True:
         img_counter += 1
 
         imgFile = cv2.imread(img_name)
-        gray = cv2.cvtColor(imgFile, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-        (thresh, im_bw) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        contours, hierarchy = cv2.findContours(im_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        canny = cv2.Canny(imgFile, 220, 250)
+        contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        im_bw = cv2.cvtColor(im_bw, cv2.COLOR_GRAY2RGB)
-        cv2.drawContours(im_bw, contours, -1, (0,255,0), 1)
-        mask = cv2.inRange(im_bw, (0, 200, 0), (20, 255, 20))
-        cv2.imshow('dst_rt', mask)
-        cv2.imwrite(img_name, mask)
+        cv2.drawContours(imgFile, contours, -1, (255, 0, 0), 1)
+        #drawLines(imgFile)
+        #mask = cv2.inRange(canny, (0, 0, 0), (250, 255, 250))
+        cv2.imshow('contours', imgFile)
+        cv2.imshow('dst_rt', canny)
+        #print(contours)
+        df = pandas.DataFrame(contours)
+        print(df)
+        df.to_csv("C:/PythonPrograms/mlproject/contourArrays/foo.data", sep='\t')
+        cv2.imwrite(img_name, canny)
         cv2.waitKey(1)
 
 cam.release()
